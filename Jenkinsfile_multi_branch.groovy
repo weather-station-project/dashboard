45789da0eb55dd338b0075710b6332980caf1b9c
David Leon <david.leon@nexthink.com>
2021-05-04 22:22:54 +0200

    jenkins file

---
+++ Jenkinsfile_multi_branch.groovy
@@ -0,0 +1,80 @@
+@Library('shared-library') _
+import com.davidleonm.WeatherStationDashboardVariables
+
+pipeline {
+  agent { label 'net-core-slave' }
+
+  stages {
+    /*stage('Prepare Python ENV') {
+      steps {
+        script {
+          setBuildStatus('pending', "${WeatherStationSensorsReaderVariables.RepositoryName}")
+
+          // Clean & Prepare new python environment
+          sh '''
+             rm -rf ENV
+             python3 -m venv ENV
+
+             ENV/bin/pip install --no-cache-dir --upgrade pip
+             ENV/bin/pip install --no-cache-dir --upgrade wheel
+             ENV/bin/pip install --no-cache-dir --upgrade setuptools
+
+             ENV/bin/pip install --no-cache-dir psycopg2 gpiozero coverage
+             '''
+        }
+      }
+    }*/
+
+    /*stage('Execute unit tests and code coverage') {
+      steps {
+        script {
+          sh """
+             ENV/bin/python -m unittest discover -s ${WORKSPACE}/WeatherStationSensorsReader
+             ENV/bin/coverage run -m unittest discover -s ${WORKSPACE}/WeatherStationSensorsReader
+             """
+        }
+      }
+    }*/
+
+    /*stage('SonarQube analysis') {
+      environment {
+        def scannerHome = tool 'Sonarqube'
+      }
+
+      steps {
+        script {
+          sh "ENV/bin/coverage xml"
+        }
+
+        withSonarQubeEnv('Sonarqube') {
+          sh "${scannerHome}/bin/sonar-scanner"
+        }
+
+        timeout(time: 10, unit: 'MINUTES') {
+          waitForQualityGate abortPipeline: true
+        }
+      }
+    }*/
+
+    stage('Deploy on staging') {
+      steps {
+        script {
+          deployContainerOnDockerHub("${WeatherStationSensorsReaderVariables.DockerHubStagingRegistryName}")
+        }
+      }
+    }
+  }
+  post {
+    success {
+      script {
+        setBuildStatus('success', "${WeatherStationSensorsReaderVariables.RepositoryName}")
+      }
+    }
+
+    failure {
+      script {
+        setBuildStatus('failure', "${WeatherStationSensorsReaderVariables.RepositoryName}")
+      }
+    }
+  }
+}
