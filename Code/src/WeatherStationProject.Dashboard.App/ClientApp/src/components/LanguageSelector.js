7fcf07bdad8bd8b05d3b323ff0f4b17c15a48e9a
David Leon <david.leon@nexthink.com>
2021-05-13 18:06:41 +0200

    removed styles

--- Code/src/WeatherStationProject.Dashboard.App/ClientApp/src/components/LanguageSelector.js
+++ Code/src/WeatherStationProject.Dashboard.App/ClientApp/src/components/LanguageSelector.js
@@ -1,6 +1,5 @@
 ﻿import React from 'react';
 import { useTranslation } from 'react-i18next';
-import './LanguageSelector.css';
 
 function LanguageSelector() {
     const { t, i18n } = useTranslation();
@@ -9,7 +8,7 @@ function LanguageSelector() {
     };
 
     return (
-        <select class="selectpicker" value={i18n.language} onChange={handleChange}>
+        <select className="form-control form-control-sm" value={i18n.language} onChange={handleChange}>
             <option value="en">{t('navmenu.language_selector.english')}</option>
             <option value="es">{t('navmenu.language_selector.spanish')}</option>
         </select>
