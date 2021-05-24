1c4e6a4ebeab952c34aae13ed35b5d75c6f2edef
David Leon <david.leon@nexthink.com>
2021-05-24 22:33:36 +0200

    feeder

---
+++ Code/tests/WeatherStationProject.Dashboard.DatabaseFeeder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherStationProject.Dashboard.DatabaseFeeder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
