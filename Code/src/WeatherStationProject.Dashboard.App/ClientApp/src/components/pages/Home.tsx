5d5aa7e31bcee4efcfc151c6f09342f1ede5eb25
David Leon <david.leon@nexthink.com>
2021-08-25 17:13:49 +0200

    home finished

--- Code/src/WeatherStationProject.Dashboard.App/ClientApp/src/components/pages/Home.tsx
+++ Code/src/WeatherStationProject.Dashboard.App/ClientApp/src/components/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
 const Home: React.FC = () => {
@@ -7,11 +8,17 @@ const Home: React.FC = () => {
     return (
         <div style={{ paddingTop: 20 }}>
             <h1>Weather Station Project - Dashboard</h1>
-            <p>{t("home.introduction")}</p>
+            <p>{t("home.introduction")}.</p>
             <p>{t("home.more_information")}<a href="https://github.com/weather-station-project/weather-station-project" target="blank">link</a>.</p>
-            <p>{t("home.technologies")}
-
-            </p>
+            <p>{t("home.technologies")}:</p>
+            <ListGroup variant="flush">
+                <ListGroup.Item>{t("home.technologies.clientapp")}.</ListGroup.Item>
+                <ListGroup.Item>{t("home.technologies.accuweather")}.</ListGroup.Item>
+                <ListGroup.Item>{t("home.technologies.apis")}.</ListGroup.Item>
+                <ListGroup.Item>{t("home.technologies.authentication")}.</ListGroup.Item>
+                <ListGroup.Item>{t("home.technologies.gateway")}.</ListGroup.Item>
+                <ListGroup.Item>{t("home.technologies.docker")}.</ListGroup.Item>
+            </ListGroup>
         </div>
     );
 }
