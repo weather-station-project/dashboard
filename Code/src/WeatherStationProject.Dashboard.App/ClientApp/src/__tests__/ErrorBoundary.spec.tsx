c6a43e335cbcf02a602561809b5af5f2397026d8
David Leon <david.leon@nexthink.com>
2021-10-05 08:50:07 +0200

    error boundary UT

---
+++ Code/src/WeatherStationProject.Dashboard.App/ClientApp/src/__tests__/ErrorBoundary.spec.tsx
@@ -0,0 +1,12 @@
+import {render, screen} from "@testing-library/react";
+import {ErrorBoundary} from "../ErrorBoundary";
+
+describe("ErrorBoundary", () => {
+    it("When_RenderingComponent_Given_No_Errors_Should_RenderExpectedContent", () => {
+        render(<ErrorBoundary><span data-testid="id"/></ErrorBoundary>)
+
+        const element = screen.queryByTestId("id")
+        expect(element).toBeInTheDocument();
+        expect(element?.tagName.toLowerCase()).toEqual("span");
+    })
+})
