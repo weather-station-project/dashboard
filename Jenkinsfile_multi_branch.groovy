1a8470b433362caf17b00a52fd346f97c47d62a1
David Leon <david.leon@nexthink.com>
2021-11-05 20:30:53 +0100

    no message

--- Jenkinsfile_multi_branch.groovy
+++ Jenkinsfile_multi_branch.groovy
@@ -45,7 +45,7 @@ pipeline {
                                   /d:sonar.links.homepage="https://github.com/weather-station-project/dashboard" \
                                   /d:sonar.links.scm="https://github.com/weather-station-project/dashboard.git" \
                                   /d:sonar.inclusions="Code/src/**/*.*" \
-                                  /d:sonar.exclusions="**/*.spec.tsx,**/*.js" \
+                                  /d:sonar.exclusions="**/*.spec.tsx,**/*.js,Code/tests/**/*.*" \
                                   /d:sonar.test.inclusions="**/*.spec.tsx,Code/tests/**/*.cs" \
                                   /d:sonar.typescript.lcov.reportPaths="${REACT_ROOT_FOLDER}/coverage/lcov.info" \
                                   /d:sonar.testExecutionReportPaths="${REACT_ROOT_FOLDER}/coverage/test-report.xml" \
