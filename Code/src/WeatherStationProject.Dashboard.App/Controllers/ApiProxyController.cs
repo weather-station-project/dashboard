b9cfa3497f1f31dc387c2b6f0a8eadce11798abe
David Leon <david.leon@nexthink.com>
2021-11-18 19:36:09 +0100

    apiproxy UTs

--- Code/src/WeatherStationProject.Dashboard.App/Controllers/ApiProxyController.cs
+++ Code/src/WeatherStationProject.Dashboard.App/Controllers/ApiProxyController.cs
@@ -16,8 +16,19 @@ namespace WeatherStationProject.Dashboard.App.Controllers
         private const string LastMeasurementsEndPoint = "/api/v1/weather-measurements/last";
         private const string AuthenticationServiceEndPoint = "/api/v1/authentication/";
 
+        private readonly HttpMessageHandler _httpHandler = new SslIgnoreClientHandler();
+
+        public ApiProxyController()
+        {
+        }
+
+        public ApiProxyController(HttpMessageHandler handler)
+        {
+            _httpHandler = handler;
+        }
+        
         [HttpGet("last")]
-        public async Task<IActionResult> LastMeasurements()
+        public async Task<ObjectResult> LastMeasurements()
         {
             var authToken = await GetAuthToken();
 
@@ -26,9 +37,9 @@ namespace WeatherStationProject.Dashboard.App.Controllers
                 : Ok(await GetLastMeasurements(authToken));
         }
 
-        private static async Task<string> GetAuthToken()
+        private async Task<string> GetAuthToken()
         {
-            using var client = new HttpClient(new SslIgnoreClientHandler());
+            using var client = new HttpClient(_httpHandler);
             var response = await client.GetAsync(AppConfiguration.AuthenticationServiceHost +
                                                  AuthenticationServiceEndPoint +
                                                  AppConfiguration.AuthenticationSecret);
@@ -39,9 +50,9 @@ namespace WeatherStationProject.Dashboard.App.Controllers
             return JsonConvert.DeserializeObject<AuthenticationToken>(jsonString).AccessToken;
         }
 
-        private static async Task<string> GetLastMeasurements(string authToken)
+        private async Task<string> GetLastMeasurements(string authToken)
         {
-            using var client = new HttpClient(new SslIgnoreClientHandler());
+            using var client = new HttpClient(_httpHandler);
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
 
             var response = await client.GetAsync(AppConfiguration.WeatherApiHost + LastMeasurementsEndPoint);
---
+++ Code/tests/WeatherStationProject.Dashboard.App.Tests/Controllers/ApiProxyControllerTest.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
+using WeatherStationProject.Dashboard.App.Controllers;
+using WeatherStationProject.Dashboard.Core.Model;
+using Xunit;
+
+namespace WeatherStationProject.Dashboard.App.Tests
+{
+    public class ApiProxyControllerTest
+    {
+        private static readonly AuthenticationToken MockAuth = new()
+        {
+            AccessToken = "test",
+            ExpiresIn = 1
+        };
+
+        [Fact]
+        public async Task When_GettingLastMeasurement_Should_Return_ExpectedResult()
+        {
+            // Arrange
+            var mockMessageHandler = new Mock<HttpMessageHandler>();
+            mockMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync((HttpRequestMessage _, CancellationToken _) =>
+                {
+                    var response = new HttpResponseMessage();
+                    response.StatusCode = HttpStatusCode.OK;
+                    response.Content = new StringContent(JsonConvert.SerializeObject(MockAuth));
+                    response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    return response;
+                });
+            var controller = new ApiProxyController(mockMessageHandler.Object);
+            Environment.SetEnvironmentVariable("AUTHENTICATION_SERVICE_HOST", "http://127.0.0.1");
+            Environment.SetEnvironmentVariable("WEATHER_API_HOST", "http://127.0.0.1");
+
+            // Act
+            var result = await controller.LastMeasurements();
+
+            // Assert
+            Assert.Equal("{\"AccessToken\":\"test\",\"ExpiresIn\":1}", result.Value);
+        }
+
+        [Fact]
+        public async Task When_GettingLastMeasurement_Given_HttpError_Should_ThrowException()
+        {
+            // Arrange
+            var mockMessageHandler = new Mock<HttpMessageHandler>();
+            mockMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync((HttpRequestMessage _, CancellationToken _) =>
+                {
+                    var response = new HttpResponseMessage();
+                    response.StatusCode = HttpStatusCode.InternalServerError;
+                    return response;
+                });
+            var controller = new ApiProxyController(mockMessageHandler.Object);
+
+            // Act
+            var result = await controller.LastMeasurements();
+            
+            // Assert
+            Assert.Equal("Auth token could not be retrieved", result.Value);
+        }
+    }
+}
