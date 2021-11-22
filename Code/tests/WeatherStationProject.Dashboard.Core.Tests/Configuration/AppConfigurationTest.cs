c32516c13402bf3cdcf68a2def3cd4bc13b3c9dd
David Leon <david.leon@nexthink.com>
2021-11-22 22:45:42 +0100

    more unit tests

---
+++ Code/tests/WeatherStationProject.Dashboard.Core.Tests/Configuration/AppConfigurationTest.cs
@@ -0,0 +1,84 @@
+using System;
+using WeatherStationProject.Dashboard.Core.Configuration;
+using Xunit;
+
+namespace WeatherStationProject.Dashboard.Core.Tests
+{
+    public class AppConfigurationTest
+    {
+        [Fact]
+        public void When_GettingDatabaseConnectionString_Should_Return_ExpectedResult()
+        {
+            // Arrange
+            const string server = "testServer";
+            const string database = "database";
+            const string user = "user";
+            const string password = "password";
+
+            Environment.SetEnvironmentVariable("SERVER", server);
+            Environment.SetEnvironmentVariable("DATABASE", database);
+            Environment.SetEnvironmentVariable("USER", user);
+            Environment.SetEnvironmentVariable("PASSWORD", password);
+
+            var expectedValue = $"Host={server};Database={database};Username={user};Password={password}";
+
+            // Act & Assert
+            Assert.Equal(AppConfiguration.DatabaseConnectionString, expectedValue);
+        }
+        
+        [Fact]
+        public void When_GettingAuthenticationSecret_Should_Return_ExpectedResult()
+        {
+            // Arrange
+            const string value = "testAuthSecret";
+            Environment.SetEnvironmentVariable("AUTHENTICATION_SECRET", value);
+
+            // Act & Assert
+            Assert.Equal(AppConfiguration.AuthenticationSecret, value);
+        }
+        
+        [Fact]
+        public void When_GettingAccuWeatherApiKey_Should_Return_ExpectedResult()
+        {
+            // Arrange
+            const string value = "testApiKey";
+            Environment.SetEnvironmentVariable("ACCUWEATHER_API_KEY", value);
+
+            // Act & Assert
+            Assert.Equal(AppConfiguration.AccuWeatherApiKey, value);
+        }
+        
+        [Fact]
+        public void When_GettingAccuWeatherLocationName_Should_Return_ExpectedResult()
+        {
+            // Arrange
+            const string value = "testLocationName";
+            Environment.SetEnvironmentVariable("ACCUWEATHER_LOCATION_NAME", value);
+
+            // Act & Assert
+            Assert.Equal(AppConfiguration.AccuWeatherLocationName, value);
+        }
+        
+        [Fact]
+        public void When_GettingAccuWeatherApiHost_Should_Return_ExpectedResult()
+        {
+            // Arrange
+            const string value = "testApiHost";
+            Environment.SetEnvironmentVariable("WEATHER_API_HOST", value);
+
+            // Act & Assert
+            Assert.Equal(AppConfiguration.WeatherApiHost, value);
+        }
+        
+        [Fact]
+        public void When_GettingAuthenticationServiceHost_Should_Return_ExpectedResult()
+        {
+            // Arrange
+            const string value = "testServiceHost";
+            Environment.SetEnvironmentVariable("AUTHENTICATION_SERVICE_HOST", value);
+
+            // Act & Assert
+            Assert.Equal(AppConfiguration.AuthenticationServiceHost, value);
+        }
+    }
+}
