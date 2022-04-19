99876d19b92d03080313afce3710b8f59b1141ca
David Leon <david.leon@nexthink.com>
2022-04-19 19:26:40 +0200

    no message

--- Jenkinsfile_multi_branch.groovy
+++ Jenkinsfile_multi_branch.groovy
@@ -63,7 +63,7 @@ pipeline {
                                                          /p:CollectCoverage=true \
                                                          /p:CoverletOutput="${COVERAGE_FOLDER_PATH}" \
                                                          /p:MergeWith="${COVERAGE_TEMP_FOLDER_PATH}coverlet.json" \
-                                                         /p:CoverletOutputFormat=\"opencover,json\"" \
+                                                         /p:CoverletOutputFormat="opencover,json" \
                                                          -m:1 )
                            """
                         sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_CREDENTIALS}"
