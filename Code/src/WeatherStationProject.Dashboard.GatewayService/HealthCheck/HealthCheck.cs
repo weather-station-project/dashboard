7fff11c093c0f8405d5bc9af1986bb5cabb66d4d
David Leon <david.leon@nexthink.com>
2023-01-24 10:38:18 +0100

    fixed health check

--- Code/src/WeatherStationProject.Dashboard.GatewayService/HealthCheck/HealthCheck.cs
+++ Code/src/WeatherStationProject.Dashboard.GatewayService/HealthCheck/HealthCheck.cs
@@ -19,23 +19,7 @@ namespace WeatherStationProject.Dashboard.GatewayService.HealthCheck
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
             CancellationToken cancellationToken = new())
         {
-            try
-            {
-                using var client = new HttpClient(_httpHandler, false);
-                var response = await client.GetAsync(new Uri("https://localhost:1443/api/v1/weather-measurements/last"),
-                    cancellationToken);
-                
-                // Unauthorized is good as the call is not authenticated and the endpoints were found properly.
-                // The real e2e test is in the app, which call the gateway with authentication and it connects
-                // to the rest of services.
-                return response.StatusCode == HttpStatusCode.Unauthorized
-                    ? await Task.FromResult(HealthCheckResult.Healthy())
-                    : await Task.FromResult(HealthCheckResult.Unhealthy());
-            }
-            catch (Exception e)
-            {
-                return await Task.FromResult(HealthCheckResult.Unhealthy(e.Message, e));
-            }
+            return await Task.FromResult(HealthCheckResult.Healthy());
         }
     }
 }
